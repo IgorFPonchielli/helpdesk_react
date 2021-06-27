ecf683ecfdf5a871a279ebc26db73356e3690d8f
henriqueheemann <henrique_heemann@estudante.sc.senai.br>
2021-06-27 15:09:30 -0300

    rotas e validação de senha e e-mail

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const app = express();
+const cors = require('cors');
+const usuarioRoute = require('./route/usuarioRoute');
+
+
+app.use(express.json());
+app.use(cors());
+app.use(usuarioRoute);
+app.listen(3334);
+
