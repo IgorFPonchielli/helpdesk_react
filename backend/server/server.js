df21d83c60ddff0879c47d3ecb55776916bb6625
Igor Ponchielli <igor.ponchielli@gmail.com>
2021-07-03 19:54:45 -0300

    ajustes de rotas

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const usersRoute = require('../routes/usuarioRoute');
+const chamadoRoute = require('../routes/chamadoRoute');
+const loginRoute = require('../routes/loginRoute');
 const categoriaRoute = require('../routes/categoriaRoute');
 
 app.use(express.json());
 app.use(cors());
 
 app.use(usersRoute);
+app.use(chamadoRoute);
+app.use(loginRoute);
 app.use(categoriaRoute);
 app.listen(3333);
